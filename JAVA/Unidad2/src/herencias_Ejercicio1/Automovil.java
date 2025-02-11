c89d6f2277292c3d7bf23ccd02b642842e1d82c1
Angelita0907 <achimon0907@g.educaand.es>
2025-02-11 21:09:51 +0100

    Cambios ejercicio Herencias

--- JAVA/Unidad2/src/herencias_Ejercicio1/Automovil.java
+++ JAVA/Unidad2/src/herencias_Ejercicio1/Automovil.java
@@ -16,6 +16,24 @@ public class Automovil extends Vehiculo {
     public void setCalificacionEcologica(String calificacionEcologica) {
         this.calificacionEcologica = calificacionEcologica;
     }
+    
+    boolean tieneLimitaciónParaCircular(String ciudad) {
+    	boolean limitado = false;
+    	
+    	if (ciudad.equals("Madrid") || ciudad.equals("Barcelona") && calificacionEcologica.equals("B") || calificacionEcologica.equals("C"))
+    	{
+    		limitado = true;
+    	}
+    	else if(ciudad.equals("Sevilla") || ciudad.equals("Valencia") && calificacionEcologica.equals("C"))
+    	{
+    		limitado = true;
+    	}
+    	else {
+    		limitado = false;
+    	}
+    	
+    	return limitado;
+    }
 
     @Override
 	public String toString() {
