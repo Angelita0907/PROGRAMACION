894e715007fc2e3e2b9355bcd4880c32094f6020
Angelita0907 <achimon0907@g.educaand.es>
2025-02-25 09:55:18 +0100

    Autoincrementales y terminar ejercicio

---
+++ JAVA/Unidad2/src/controlador/GestionaPersona2.java
@@ -0,0 +1,14 @@
+package controlador;
+
+import modelo.Persona;
+
+public class GestionaPersona2 {
+
+	public static void main(String[] args) {
+		/*Persona p1 = new Persona("Pepe", 20);
+		System.out.println(p1);
+		Persona p2 = new Persona("Pepa", 18);		
+		System.out.println(p2);*/
+	}
+
+}
