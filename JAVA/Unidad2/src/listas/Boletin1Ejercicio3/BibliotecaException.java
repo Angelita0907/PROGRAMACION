07272274b1e01c82d58fc062ea13a1623f54aea5
Angelita0907 <achimon0907@g.educaand.es>
2025-03-26 21:17:29 +0100

    ejercicio listas

---
+++ JAVA/Unidad2/src/listas/Boletin1Ejercicio3/BibliotecaException.java
@@ -0,0 +1,12 @@
+package listas.Boletin1Ejercicio3;
+
+public class BibliotecaException extends Exception{
+
+	public BibliotecaException(String mensaje) {
+		super(mensaje);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
--- JAVA/Unidad2/src/listas/Boletin1Ejercicio3/gestionaBiblioteca.java
+++ JAVA/Unidad2/src/listas/Boletin1Ejercicio3/gestionaBiblioteca.java
@@ -20,8 +20,49 @@ public class gestionaBiblioteca {
 			System.out.println("5. Mostrar información detallada de un libro: autor, género, año de publicación, ejemplares disponibles y ejemplares prestados");
 			System.out.println("6. Buscar libros por título o autor.");		
 			System.out.println("7. Salir del programa.");
+			
+			System.out.println("Indica opción: ");
+			opcion = sc.next();
 
+			switch (opcion) {
+			case "1": {
+				//funcion agregar libro
+			}
+			break;
+			case "2":{
+				//pestar un libro
+			}
+			break;
+			case "3":{
+				//devolver un libro
+			}
+			break;
+			case "4": {
+				//mostrar libros en el invertario
+			}
+			break;
+			case "5": {
+				//mostrar informacion del libro
+			}
+			break;
+			case "6":{
+				//buscar libros por titulo o autor
+			}
+			break;
+			case "7":{
+				salir = true;
+				System.out.println("Saliendo del programa");
+			}
+			break;
+			default:
+				
+			}
+			
 		}
+		
+		Libro l1 = new Libro("Poppy", "R. F. Kuang", "Fantasía", 2018, Estado.LIBRE);
+		
+		
 		
 	}
 
