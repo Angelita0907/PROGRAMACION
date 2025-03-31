6bbeb19af04fd96adb965be59a91e2bc9c62f12a
Angelita0907 <achimon0907@g.educaand.es>
2025-03-31 19:11:59 +0200

    ejercicio equipos

---
+++ JAVA/Unidad2/Listas/Repositorio/DeportivosException.java
@@ -0,0 +1,10 @@
+package Repositorio;
+
+public class DeportivosException extends Exception {
+
+	public DeportivosException(String mensaje) {
+		super(mensaje);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
